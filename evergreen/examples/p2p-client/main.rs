0b28b7b9bde0bf93a1e53add7691789a364ca6f5
Dungen <104450078+DungenRobot@users.noreply.github.com>
2026-06-02 12:52:52 -0400

    Updated examples
    - Cleared out client and server examples.
    - Created basic Iroh p2p client example.
    - Created iroh chat example

--- evergreen/examples/p2p-client/main.rs
+++ evergreen/examples/p2p-client/main.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Anders Olsen
+// Copyright (c) 2026 Anders Olsen
 //
 // Permission is hereby granted, free of charge, to any person obtaining 
 // a copy of this software and associated documentation files (the "Software"), 
@@ -17,15 +17,89 @@
 // AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
 // THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-use std::error::Error;
+use std::str::FromStr;
 
+use anyhow::Context;
+use iroh::{Endpoint, EndpointAddr, PublicKey, endpoint::{Connection, presets}};
+
+
+
+const ALPN: &'static [u8; 15] = b"evergreen/0.1.0";
 
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>>{
+async fn main() -> anyhow::Result<()>{
+
+    let endpoint = Endpoint::builder(presets::N0).alpns(vec![ALPN.to_vec()]).bind().await?;
+
+    endpoint.online().await;
+
+    println!("{}", endpoint.id());
+
+    match std::env::args().len() {
+        1 => {
+            init_recv(endpoint).await
+        },
+        2 => {
+
+            let public_key_string = &std::env::args().collect::<Vec<String>>()[1];
+
+            let public_key = PublicKey::from_str(public_key_string)?;
+
+            //public_key.is_v
+
+            //println!()
+
+            let addr = EndpointAddr::new(public_key);
+
+            init_send(endpoint, addr).await
+        }
+        _ => {
+            println!("D:");
+            Ok(())
+        }
+    }
+}
 
 
 
+async fn init_send(endpoint: Endpoint, addr: EndpointAddr) -> anyhow::Result<()> {
+
+    let connection = endpoint.connect(addr, ALPN).await?;
+
+    let mut send_stream = connection.open_uni().await?;
+
+    send_stream.write_all(b"fuck around and find out").await?;
+
+    send_stream.finish()?;
+
+    let connection_error = connection.closed().await;
+
+    println!("{connection_error}");
+
+    endpoint.close().await;
+
+    Ok(())
+}
+
+
+async fn init_recv(endpoint: Endpoint) -> anyhow::Result<()> {
+
+    let connection = endpoint.accept().await.context("Endpoint closed")?.await?;
+
+    println!("Accepted connection");
+
+    let mut recv_stream = connection.accept_uni().await?;
+
+    println!("Accepted stream.");
+
+    let bytes = recv_stream.read_to_end(300).await?;
+
+    println!("{}", String::from_utf8(bytes)?);
+
+    connection.close(1u8.into(), b"done");
+
+    endpoint.close().await;
 
     Ok(())
 }
--- evergreen/examples/server/main.rs
+++ evergreen/examples/server/main.rs
@@ -17,69 +17,9 @@
 // AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
 // THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-use std::error::Error;
-
-use libp2p::identity::Keypair;
-use firesidexr_evergreen::server;
-use tracing_subscriber::EnvFilter;
-
-
-use firesidexr_evergreen::types::*;
-
-
-
-
-
-use libp2p::multiaddr;
-use std::net::Ipv6Addr;
-
-
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>>{
-
-    let _ = tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env()).try_init();
-
-    let keypair = Keypair::generate_ed25519();
-    let addr = libp2p::Multiaddr::empty().with(multiaddr::Protocol::Ip6(Ipv6Addr::UNSPECIFIED)).with(multiaddr::Protocol::QuicV1);
-    //let addr: libp2p::Multiaddr = "/ip6/::/udp/0/quic-v1".parse()?;
-
-    let (mut server, mut server_handle) = 
-        server::untrusted::Network::new_server(keypair, addr).expect("Could not create server");
-
-    let handle = tokio::task::spawn(async move { server.run().await; });
-
-    println!("Server started");
-
-    loop {
-        println!("Loop");
-
-        if handle.is_finished() { break }
-
-        tokio::select! {
-            Some(resp) = server_handle.get_event() => match resp {
-                Response::Network(NetworkUpdate::AliveWithAddr(addr)) => {
-                    println!("Alive with addr: {addr}")
-                },
-
-                Response::Network(NetworkUpdate::Disconnected) => {
-                    println!("Disconnected");
-                    break;
-                },
-
-                Response::Client(resp) => {
-                    let peer = resp.peer;
-                    let data = resp.data;
-                    println!("{peer}, {data:?}");
-                },
-
-                _ => (),
-            }
-        }
-    }
-
-
-    let _ = handle.await?;
+async fn main() -> anyhow::Result<()>{
 
     Ok(())
 }
